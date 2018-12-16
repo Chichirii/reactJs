cd6a43c12fb00cef4c9ad045dd0ee2dfa8e6d638
Chichirii <36487519+Chichirii@users.noreply.github.com>
2018-12-16 03:13:40 -0500

    Created titleBar for webpage

--- movies_search/src/App.js
+++ movies_search/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="titleBar">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="50" src="movieDB_icon.svg"/>
+              </td>
+              <td>
+                  <h3>MoviesDB Search</h3>
+              </td>
+              <td width="8"/>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
