8bfcd9da6ad7f444665af0b6a534ce8da578ef3f
Chichirii <36487519+Chichirii@users.noreply.github.com>
2019-01-01 12:33:06 -0500

    added some gulp files

--- first_website/gulpfile.js
+++ first_website/gulpfile.js
@@ -12,24 +12,24 @@ let changed = require('gulp-changed');
 // ~SCSS/CSS
  */
 
-let SCSS_SRC = '.src/Assets/scss/**/*.scss';
-let SCSS_DEST = './src/ASsets/css';
+let SCSS_SRC = './src/Assets/scss/**/*.scss';
+let SCSS_DEST = './src/Assets/css';
 
 // Compile SCSS
-gulp.task('compile_scss', function() {
-    gulp.src(SCSS_SRC)
-      .pipe(sass().on('error', sass.logError))
-      .pipe(minifyCSS())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(changed(SCSS_DEST))
-      .pipe(gulp.dest(SCSS_DEST));
+gulp.task('compile', gulp.series('compile_scss', function(){
+  gulp.src(SCSS_SRC)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(changed(SCSS_DEST))
+    .pipe(gulp.dest(SCSS_DEST));
 
-});
+}));
 
 // detect changes in SCSS
-gulp.task('watch_scss', function(){
+gulp.task('watch', gulp.series('watch_scss', function(){
   gulp.watch(SCSS_SRC, ['compile_scss']);
-});
+}));
 
 // run tasks
-gulp.task('default', ['watch_scss']);
+gulp.task('default', gulp.series('watch_scss'));
